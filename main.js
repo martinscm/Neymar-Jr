b9da5de4948595a9fd61024298c9a11ba1c838ad
martinscm <163418840+martinscm@users.noreply.github.com>
2024-03-15 10:06:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById ("imagem"). src = "https://i.pinimg.com/736x/c5/4b/19/c54b19d470268290c66a270c58588daf.jpg"
+}
+function mudarimagemdentro()
+{
+    document.getElementById ("imagem").src = "https://p2.trrsf.com/image/fget/cf/1200/1600/middle/images.terra.com/2023/08/09/neymar-ublw6asdo31g.png" 
+}
